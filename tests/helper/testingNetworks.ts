bccff55eba6529c75a7098ad2f3192098d711b5c
jeremy-babylonchain <jeremy.tsang@babylonchain.io>
2024-06-26 12:39:44 -0700

    update testingNetworks

--- tests/helper/testingNetworks.ts
+++ tests/helper/testingNetworks.ts
@@ -1,17 +1,21 @@
 import * as bitcoin from "bitcoinjs-lib";
 import DataGenerator from "./dataGenerator";
 
-const testingNetworks = [
-  {
-    networkName: "mainnet",
-    network: bitcoin.networks.bitcoin,
-    dataGenerator: new DataGenerator(bitcoin.networks.bitcoin),
-  },
-  {
-    networkName: "testnet",
-    network: bitcoin.networks.testnet,
-    dataGenerator: new DataGenerator(bitcoin.networks.testnet),
-  },
+interface NetworkConfig {
+  networkName: string;
+  network: bitcoin.Network;
+  dataGenerator: DataGenerator;
+}
+
+const createNetworkConfig = (networkName: string, network: bitcoin.Network): NetworkConfig => ({
+  networkName,
+  network,
+  dataGenerator: new DataGenerator(network),
+});
+
+const testingNetworks: NetworkConfig[] = [
+  createNetworkConfig("mainnet", bitcoin.networks.bitcoin),
+  createNetworkConfig("testnet", bitcoin.networks.testnet),
 ];
 
 export default testingNetworks;
