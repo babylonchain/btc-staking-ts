f7507065f1462b83129daf6ca9a4487fa502f195
wjrjerome <wjrjerome@babylonchain.io>
2024-07-11 12:55:47 +1000

    revert the multi-FP test cases

--- tests/utils/stakingScript.test.ts
+++ tests/utils/stakingScript.test.ts
@@ -31,7 +31,7 @@ describe("stakingScript", () => {
       expect(() =>
         new StakingScriptData(
           pk1, // Staker Pk
-          [invalidPk], // Finality Provider Pks
+          [pk2, invalidPk], // Finality Provider Pks
           [pk3, pk4, pk5], // covenant Pks
           2,
           stakingTimeLock,
@@ -59,7 +59,7 @@ describe("stakingScript", () => {
       expect(() =>
         new StakingScriptData(
           pk1, // Staker Pk
-          [pk2], // Finality Provider Pks
+          [pk2, pk3], // Finality Provider Pks
           [pk4, invalidPk, pk5], // covenant Pks
           2,
           stakingTimeLock,
